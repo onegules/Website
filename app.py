ac47e6ce5ff76aa2431363669ca4f356d1d40779
Ovi <Chrono.hi@gmail.com>
2020-06-20 12:43:51 -0400

    feat: Add gevent functionality to server the app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import Flask, redirect, url_for, request, render_template, Response, jsonify
+import flask, redirect, url_for, request, render_template, Response, jsonify
+from gevent.pywsgi import WSGIServer
 
 # Declare a Flask app
 app = Flask(__name__)
@@ -9,3 +10,8 @@ print(' Loading complete. Check http://127.0.0.1:5000/')
 def index():
     # Main page
     return render_template("index.html")
+
+if __name__ == '__main__':
+    # Server the app with gevent
+    http_server = WSGIServer(('0.0.0.0', 5000), app)
+    http_server.serve_forever()
